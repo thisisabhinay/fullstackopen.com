d9fd177703909b45eba59eb8b8f68bc2043b524e
thisisabhinay <a8h1nay@gmail.com>
2019-12-26 00:44:37 +0530

    Completes first example project in part 1

--- chapter-examples/part1/src/index.js
+++ chapter-examples/part1/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => (
-    <div>
-        <p>Hello {props.name}, you are {props.age} years old.</p>
-    </div>
-);
+const Hello = ({name, age}) => {
+    const bornYear = () => new Date().getFullYear() - age;
+
+    return(
+        <div>
+            <p>
+                Hello {name}, you are {age} years old.
+            </p>
+            <p>
+                So you were probably born in {bornYear()}
+            </p>
+        </div>
+    );
+};
 
 const App = () => {
     const name = "Peter";
