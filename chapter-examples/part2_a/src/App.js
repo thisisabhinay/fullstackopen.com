4f373f218c0e3f012d918e1788cccb8c98d01cff
thisisabhinay <a8h1nay@gmail.com>
2019-12-29 00:02:47 +0530

    Completes example code of 'Part 2A'

---
+++ chapter-examples/part2_a/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Note from './components/Note';
+
+const App = ({notes}) => {
+    const rows = () => notes.map((note) => <Note key={note.id} note={note} />);
+    console.log("Rendering App...");
+    return(
+        <>
+            <h1>Notes</h1>
+            <ul>
+                {rows()}
+            </ul>
+        </>
+    );
+}
+
+export default App;
