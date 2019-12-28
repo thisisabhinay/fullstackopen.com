741b4e3cc6a0099e9d9f8df63aceda79f1a05495
thisisabhinay <a8h1nay@gmail.com>
2019-12-28 16:59:15 +0530

    Completes exercise 1.6 'unicafe step1'

--- exercises/Part 1/unicafe/src/index.js
+++ exercises/Part 1/unicafe/src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+const Heading = ({text}) => (<h1>{text}</h1>);
+const Button = ({onClick, label}) => (<button onClick={onClick}>{label}</button>);
+const Stat = ({text, count}) => (<p>{text}: <strong>{count}</strong></p>);
+
+const App = () =>{
+    // Defining component's states with React hook
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+
+    return(
+        <>
+            <Heading text="Give Feedback" />
+            <Button onClick={() => setGood(good + 1)} label="Good" />
+            <Button onClick={() => setNeutral(neutral + 1)} label="Neutral" />
+            <Button onClick={() => setBad(bad + 1)} label="Bad" />
+
+            <hr />
+            <Heading text="Statics" />
+            <Stat text="Good" count={good} />
+            <Stat text="Neutral" count={neutral} />
+            <Stat text="Bad" count={bad}  />
+        </>
+    );
+}
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
