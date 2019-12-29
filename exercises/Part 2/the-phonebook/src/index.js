c87cfd1fba0c0830ca35d08c85e1c96d73d3c1ea
thisisabhinay <a8h1nay@gmail.com>
2019-12-29 19:04:21 +0530

    Completes exercise 2.9 'The Phonebook Step4'

--- exercises/Part 2/the-phonebook/src/index.js
+++ exercises/Part 2/the-phonebook/src/index.js
@@ -8,10 +8,14 @@ import './index.css';
 
 const App = () => {
     const [ persons, setPersons] = useState([
-        { name: 'Arto Hellas' , contact: '040-1234567', id: 1 }
+        { id: 1, name: 'Arto Hellas', number: '040-123456' },
+        { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+        { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+        { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
     ]); 
     const [ newName, setNewName ] = useState('');
     const [ newNumber, setNewNumber ] = useState('');
+    const [ beforeSearchData, setBeforeSearchData ] = useState(persons);
 
     // Sync input entered with component inputs state
     const newNameChange = (event) => setNewName(event.target.value);
@@ -19,11 +23,11 @@ const App = () => {
     
     // Generates list of contacts in the state
     const contacts = () => {
-		return persons.map((person, i) => 
+		return persons.map((person) => 
             <Item 
                 key={person.id} 
                 name={person.name} 
-                contact={person.contact}
+                contact={person.number}
             />
         );
 	};
@@ -41,19 +45,39 @@ const App = () => {
 
         const newPerson = {
             name: newName,
-            contact: newNumber,
+            number: newNumber,
             created_on: new Date().toISOString(),
             id: persons.length + 1
         }
 
         setPersons(persons.concat(newPerson));
+        setBeforeSearchData(persons.concat(newPerson));
         setNewName('');
         setNewNumber('');
-	};
+    };
+    
+    // Search functionality
+    const search = (event) => {
+        const str = event.target.value.toUpperCase();
+        if(str === '') {
+            setPersons(beforeSearchData);
+            return;
+        }
+
+        const result = persons.filter(({name}) => name.toUpperCase().search(str) !== -1);
+        setPersons(result);
+    } 
 
     return(
         <>
             <Heading text="Phonebook" />
+            <InlineInput 
+                label="Search: "
+                inputChangeHandle={search}
+            />
+            <hr/>
+
+            <Heading text="Create New Contact" />
             <form onSubmit={onSubmit}>
                 <InlineInput 
                     label="Name: "
@@ -68,6 +92,7 @@ const App = () => {
                 <Button type="submit" label="Save" />
             </form>
             <hr />
+
             <Heading text="Numbers" />
             <table>
                 <tbody>
