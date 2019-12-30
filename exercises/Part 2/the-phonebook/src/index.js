3b1a0b05577546f3ef96524d335346e219942dc8
thisisabhinay <a8h1nay@gmail.com>
2019-12-30 18:54:13 +0530

    Completes exercise 2.11 'The Phonebook Step6'

--- exercises/Part 2/the-phonebook/src/index.js
+++ exercises/Part 2/the-phonebook/src/index.js
@@ -1,21 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import Item from './components/Item';
 import Form from './components/Form'
 import Contacts from './components/Contacts'
 import Search from './components/Search';
+import axios from 'axios';
 import './index.css';
 
 const App = () => {
-    const [ persons, setPersons] = useState([
-        { id: 1, name: 'Arto Hellas', number: '040-123456' },
-        { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
-        { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
-        { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]); 
+    const [ persons, setPersons] = useState([]); 
     const [ newName, setNewName ] = useState('');
     const [ newNumber, setNewNumber ] = useState('');
     const [ beforeSearchData, setBeforeSearchData ] = useState(persons);
+
+    // Using effect hok to fetch initial state from server
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data);
+                setBeforeSearchData(response.data);
+            })
+    }, []);
 
     // Sync input entered with component inputs state
     const newNameChange = (event) => setNewName(event.target.value);
